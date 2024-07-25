d6898a7c92dd765af5139a6f77c0d8c686fede32
Danila <easyezyes@gmail.com>
2024-07-25 11:10:24 +0300

    chore(slider): init slider component

---
+++ src/components/Slider.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MySlider = styled.div`
+    
+`
+
+
+interface ISlider {
+
+}
+
+
+
+const Slider: React.FC<ISlider> = ({}) => {
+  return (
+    <MySlider>
+      
+    </MySlider>
+  )
+}
+
+
+
+
+export default Slider
